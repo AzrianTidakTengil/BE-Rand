7a629917bad154e17438269249f7d0567e5c2dda
Hikoyu <azrianpintar@gmail.com>
2026-07-29 13:13:43 +0700

    fix: listen port

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Mengizinkan akses khusus dari frontend Next.js
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server is running on port: ${port}`);
 }
 bootstrap();
