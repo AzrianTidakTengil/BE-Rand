82964a27f4cd0120f4ef1f52b180778b00b9bff5
Hikoyu <azrianpintar@gmail.com>
2026-07-29 17:35:39 +0700

    fix: playground false

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,7 @@ import { CalendarModule } from './calendar/calendar.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
     }),
     TasksModule,
     EventsModule,
