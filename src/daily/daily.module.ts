2609cac32e914acaae687cbb01932931644fcce6
Hikoyu <azrianpintar@gmail.com>
2026-08-03 20:30:47 +0700

    fix: relation provider

--- src/daily/daily.module.ts
+++ src/daily/daily.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { DailyService } from './daily.service';
 import { DailyResolver } from './daily.resolver';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AppService } from '../app.service';
+import { TaskDaysService } from '../task-days/task-days.service';
+import { TasksService } from '../tasks/tasks.service';
+import { CalendarService } from '../calendar/calendar.service';
+import { WeeklyService } from '../weekly/weekly.service';
 
 @Module({
-  providers: [DailyService, DailyResolver],
+  providers: [
+    DailyService,
+    DailyResolver,
+    AppService,
+    TaskDaysService,
+    TasksService,
+    CalendarService,
+    WeeklyService,
+  ],
   imports: [PrismaModule],
+  exports: [DailyService],
 })
 export class DailyModule {}
